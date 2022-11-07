44d7a498381095237a7bcc35ad77bb6da94b8475
samuelfooks <samuel.fooks@gmail.com>
2022-11-07 22:49:58 +0100

    final stuff

--- Deployment/callAPI.py
+++ Deployment/callAPI.py
@@ -5,6 +5,6 @@ import json
 house_dict={'typeOfProperty' : 'HOUSE', 'stateOfBuilding' : 'GOOD', 'epcScore' : 'C', 'postalCode' : 9000, 'province' :'East Flanders', 'numberOfBedrooms' : 4, 'livableArea' : 160, 'surfaceAreaOfPlot' : 450}
 appt_dict={'typeOfProperty' : 'APARTMENT', 'stateOfBuilding' : 'GOOD', 'epcScore' : 'C', 'postalCode' : 9000, 'province' : 'East Flanders', 'numberOfBedrooms' : 4, 'livableArea' : 160, 'surfaceAreaOfPlot' : 450}
 
-pred= requests.post('https://immo-web-price-predictor-api.onrender.com/predict', data = house_dict)
+pred= requests.post('http://127.0.0.1:5000/predict', data = house_dict)
 
 print(pred.text)
