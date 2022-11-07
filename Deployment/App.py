298d81544e569d09bfeabc20073a8be9742e26f3
samuelfooks <samuel.fooks@gmail.com>
2022-11-07 20:28:14 +0100

    app.py response

--- Deployment/App.py
+++ Deployment/App.py
@@ -18,10 +18,10 @@ def predictor():
             price = predictor_function(dict(request.form))
             price=jsonify(price)
             response = '200 OK'
-            return(response + '\n' + price)
+            return(response + '\n' + str(price))
         else:
             response = '400 BAD REQUEST'
-
+            return response
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
