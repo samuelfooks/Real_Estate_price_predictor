b61512f796bb090244c301687ff6af01c70aaeb8
samuelfooks <samuel.fooks@gmail.com>
2022-11-08 12:15:49 +0100

    change callAPI

--- Deployment/callAPI.py
+++ Deployment/callAPI.py
@@ -3,8 +3,8 @@ import requests
 import json
 
 house_dict={'typeOfProperty' : 'HOUSE', 'stateOfBuilding' : 'GOOD', 'epcScore' : 'C', 'postalCode' : 9000, 'province' :'East Flanders', 'numberOfBedrooms' : 4, 'livableArea' : 160, 'surfaceAreaOfPlot' : 450}
-appt_dict={'typeOfProperty' : 'APARTMENT', 'stateOfBuilding' : 'GOOD', 'epcScore' : 'C', 'postalCode' : 9000, 'province' : 'East Flanders', 'numberOfBedrooms' : 4, 'livableArea' : 160, 'surfaceAreaOfPlot' : 450}
+appt_dict={'typeOfProperty' : 'APARTMENT', 'stateOfBuilding' : 'AS_NEW', 'epcScore' : 'B', 'postalCode' : 1090, 'province' : 'East Flanders', 'numberOfBedrooms' : 1, 'livableArea' : 75, 'surfaceAreaOfPlot' : 450}
 
-pred= requests.post('http://127.0.0.1:5000/predict', data = house_dict)
+pred= requests.post('https://immo-web-price-predictor-api.onrender.com/predict', data = appt_dict)
 
 print(pred.text)
